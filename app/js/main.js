90e24b3e47c23424eff08bd6e6ada4d0c0c27a6b
XeindsTwo <125365638+XeindsTwo@users.noreply.github.com>
2024-10-31 22:22:10 +0300

    page contact, whitepaper page

--- app/js/main.js
+++ app/js/main.js
@@ -4,7 +4,7 @@ ecosystem();
 
 const html = document.documentElement;
 const menuBtn = document.querySelector('.menu-btn');
-const headerNav = document.querySelector('.header__nav.mobile');
+const headerNav = document.querySelector('.header__mobile');
 const anchors = document.querySelectorAll('a.header__link.mobile');
 
 menuBtn.addEventListener('click', () => {
@@ -21,7 +21,7 @@ function scrollToTarget(targetId) {
     headerNav.classList.remove('active');
     menuBtn.classList.remove('active');
     setTimeout(() => {
-      const targetOffset = targetSection.offsetTop - 80;
+      const targetOffset = targetSection.offsetTop - 50;
       window.scrollTo({top: targetOffset, behavior: 'smooth'});
     }, 400);
   }
@@ -44,17 +44,17 @@ for (const anchor of anchors) {
 
 document.querySelectorAll(".desktop").forEach(link => {
   link.addEventListener("click", function (event) {
-    event.preventDefault(); // Отключаем стандартный якорный переход
+    event.preventDefault();
 
-    const targetId = link.getAttribute("href").slice(1); // Получаем ID целевой секции
+    const targetId = link.getAttribute("href").slice(1);
     const targetSection = document.getElementById(targetId);
 
     if (targetSection) {
-      const offsetTop = targetSection.getBoundingClientRect().top + window.scrollY - 40; // Вычисляем позицию с отступом
+      const offsetTop = targetSection.getBoundingClientRect().top + window.scrollY - 40;
 
       window.scrollTo({
         top: offsetTop,
-        behavior: "smooth" // Плавный скролл
+        behavior: "smooth"
       });
     }
   });
